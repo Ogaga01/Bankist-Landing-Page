fcd543f2a87a7dfc7eb5c74289c0172ee3897a97
Ogaga <iyaraogaga69@gmail.com>
2022-10-06 17:24:07 +0100

    Add smooth scroll

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-/// ////////////////////////////////////
-// Modal window
+// const { contains } = require("micromatch");
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const navLinks = document.querySelector('.nav__links');
+const navLink = document.querySelectorAll('.nav__link');
+
+/// ////////////////////////////////////
+// Modal window
 
 const openModal = () => {
   modal.classList.remove('hidden');
@@ -16,15 +20,25 @@ const closeModal = () => {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach((btn) => {
+btnsOpenModal.forEach(btn => {
   btn.addEventListener('click', openModal);
 });
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', (e) => {
+document.addEventListener('keydown', e => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
+
+navLinks.addEventListener('click', (e) => {
+  e.preventDefault()
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href')
+    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+  }
+})
+
+
