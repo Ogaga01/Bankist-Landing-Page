7a60b69c90a1a9d736a1a157316ce9a35a1d0996
Ogaga <iyaraogaga69@gmail.com>
2022-10-07 05:25:56 +0100

    Fix linters errors

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const navLinks = document.querySelector('.nav__links');
-const navLink = document.querySelectorAll('.nav__link');
+// const navLink = document.querySelectorAll('.nav__link');
 const tabContainer = document.querySelector('.operations__tab-container');
 const btnTab = document.querySelectorAll('.operations__tab');
 const content = document.querySelectorAll('.operations__content');
-const nav = document.querySelector('.nav')
+const nav = document.querySelector('.nav');
 
 /// ////////////////////////////////////
 // Modal window
@@ -24,61 +24,60 @@ const closeModal = () => {
   overlay.classList.add('hidden');
 };
 
-btnsOpenModal.forEach(btn => {
+btnsOpenModal.forEach((btn) => {
   btn.addEventListener('click', openModal);
 });
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', (e) => {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
   }
 });
 
 navLinks.addEventListener('click', (e) => {
-  e.preventDefault()
+  e.preventDefault();
   if (e.target.classList.contains('nav__link')) {
-    const id = e.target.getAttribute('href')
-    document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
-})
+});
 
 tabContainer.addEventListener('click', (e) => {
-  e.preventDefault()
+  e.preventDefault();
   const btn = e.target.closest('.operations__tab');
 
-  if (!btn) return
+  if (!btn) return;
 
   btnTab.forEach((el) => {
     el.classList.remove('operations__tab--active');
-  })
+  });
   btn.classList.add('operations__tab--active');
 
   content.forEach((con) => {
     con.classList.remove('operations__content--active');
-  })
+  });
   document
     .querySelector(`.operations__content--${btn.dataset.tab}`)
     .classList.add('operations__content--active');
-})
+});
 
 function handleNavLink(e) {
-  e.preventDefault()
-  
+  e.preventDefault();
+
   if (e.target.classList.contains('nav__link')) {
-    const link = e.target
+    const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
     siblings.forEach((sibling) => {
-      if (sibling !== link)
-      {sibling.classList.toggle('opaque')}
-    })
-    logo.classList.toggle('opaque')
+      if (sibling !== link) { sibling.classList.toggle('opaque'); }
+    });
+    logo.classList.toggle('opaque');
   }
 }
 
-nav.addEventListener('mouseover', handleNavLink)
-nav.addEventListener('mouseout', handleNavLink)
+nav.addEventListener('mouseover', handleNavLink);
+nav.addEventListener('mouseout', handleNavLink);
